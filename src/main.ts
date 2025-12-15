a3a70bd58766ab1d66c931c85e7a2a91fab2752c
Abdulaziz Alwaalan <AAlwaalan@masdr.sa>
2025-12-16 01:52:00 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,13 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+  const config = new DocumentBuilder()
+    .setTitle('Courier Service API')
+    .setDescription('Unified API for shipping integrations (Aramex, SMSA)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
